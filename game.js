8f1efc530ebcb1cc6bdf7b0816b62218066da0a6
Maycon <mayconds000@gmail.com>
2016-07-10 00:14:01 -0300

    correções

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 ;(function () {
+  const ctx = document.getElementById('canvas').getContext('2d');
   class Random {
     static get(inicio, final) {
       return Math.floor(Math.random() * final) + inicio;
@@ -18,7 +19,9 @@
     }
 
     static generate() {
-      return new Food(Random.get(0, 499), Random.get(0, 299));
+      const x = Random.get(5, 590);
+      const y = Random.get(5, 390);
+      return new Food(x, y);
     }
   }
 
@@ -26,163 +29,202 @@
     constructor(x, y) {
       this.x = x;
       this.y = y;
+      this.speed = 10;
       this.width = 10;
       this.height = 10;
-      this.back = null;
-    }
-
-    draw() {
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-      if (this.hasBack()) {
-        this.back.draw();
-      }
-    }
-
-    add() {
-      if (this.hasBack()) return this.back.add();
-      this.back = new Square(this.x, this.y);
-      this.draw();
-    }
-    hasBack() {
-      return this.back !== null;
+      this.lastSquare = null;
     }
 
     copy() {
       if (this.hasBack()) {
-        this.back.copy();
-        this.back.x = this.x;
-        this.back.y = this.y;
+        this.lastSquare.copy();
+        this.lastSquare.x = this.x;
+        this.lastSquare.y = this.y;
       }
     }
 
     right() {
       this.copy();
-      this.x += 10;
+      this.x += this.speed;
     }
     left() {
       this.copy();
-      this.x -= 10;
+      this.x -= this.speed;
     }
     up() {
       this.copy();
-      this.y -= 10;
+      this.y -= this.speed;
     }
     down() {
       this.copy();
-      this.y += 10;
+      this.y += this.speed;
     }
 
-    hit(head, second = false) {
-      if (this === head && !this.hasBack()) return false;
-      if (this === head) return this.back.hit(head, true);
+    addNew() {
+      if (this.hasBack()) {
+        this.lastSquare.addNew();
+      } else {
+        this.lastSquare = new Square(this.x - (this.width + 5), this.y);
+      }
+    }
 
-      if (segundo && !this.hasBack()) return false;
-      if (segundo) return this.back.hit(head);
+    hasBack() {
+      return this.lastSquare !== null;
+    }
+
+    draw() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+      if (this.hasBack()) this.lastSquare.draw();
+    }
+
+    hit(square, second = false) {
+      if (square === this && !this.hasBack()) return false;
+      if (square === this) return this.lastSquare.hit(square, true);
+
+      if (second && !this.hasBack()) return false;
+      if (second) return this.lastSquare.hit(square);
 
       //Is not a head and not is the second
-      if (this.hasBack()) {
-        return snakeHit(this, head) || this.back.hit(head);
-      }
+      if (this.hasBack()) return snakeHit(this, square) || this.lastSquare.hit(square);
 
       //Is not a head and not is the second, but is the last
-      return snakeHit(this, head);
+      return snakeHit(this, square);
+    }
+
+    hitBorder() {
+      return this.x > 490 || this.x < 0 || this.y > 290 || this.y < 0;
     }
 
   }
 
   class Snake {
     constructor() {
-      this.head = new Square(100, 0);
+      this.head = new Square(0, 0);
       this.draw();
+      this.eat();
+      this.eat();
+      this.eat();
+      this.eat();
+      this.eat();
+      this.eat();
+      this.eat();
       this.direction = "right";
-      this.head.add();
-      this.head.add();
-      this.head.add();
+    }
+
+    right() {
+      if (this.direction == "left") return;
+      this.direction = "right";
+    }
+    left() {
+      if (this.direction == "right") return;
+      this.direction = "left";
+    }
+    up() {
+      if (this.direction == "down") return;
+      this.direction = "up";
+    }
+    down() {
+      if (this.direction == "up") return;
+      this.direction = "down";
+    }
+
+    eat() {
+      this.head.addNew();
+    }
+
+    move() {
+      if (this.direction === "up") return this.head.up();
+      if (this.direction === "down") return this.head.down();
+      if (this.direction === "left") return this.head.left();
+      if (this.direction === "right") return this.head.right();
     }
 
     draw() {
       this.head.draw();
     }
 
-    right() {
-      this.direction = "right";
-    }
-    left() {
-      this.direction = "left";
-    }
-    up() {
-      this.direction = "up";
-    }
-    down() {
-      this.direction = "down";
-    }
-
-    move() {
-      if (this.direction === "up") return this.head.up();
-      if (this.direction === "down") return this.head.down();
-      if (this.direction === "right") return this.head.right();
-      if (this.direction === "left") return this.head.left();
-    }
-
-    eat() {
-      this.head.add();
-    }
-
     dead() {
-      return this.head.hit(this.head);
+      return this.head.hit(this.head) || this.head.hitBorder();
     }
   }
-
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
 
   const snake = new Snake();
   let foods = [];
 
-  window.addEventListener("keydown", function (event) {
-    event.defaultPrevented();
-    if (event.keyCode === 40) return snake.down();
-    if (event.keyCode === 39) return snake.right();
-    if (event.keyCode === 38) return snake.up();
-    if (event.keyCode === 37) return snake.left();
-
+  window.addEventListener("keydown", event => {
+    if (event.keyCode > 36 && event.keyCode < 41) event.defaultPrevented;
+    if (event.keyCode == 40) snake.down();
+    if (event.keyCode == 39) snake.right();
+    if (event.keyCode == 38) snake.up();
+    if (event.keyCode == 37) snake.left();
     return false;
   });
 
-  setInterval(function () {
-    snake.move();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    snake.draw();
-    drawFood();
-  }, 1000 / 5);
-
-  setInterval(function () {
-    let food = Food.generate();
+  setInterval(() => {
+    const food = Food.generate();
     foods.push(food);
-
-    setTimeout(function () {
-      //Elimina a comida
+    setTimeout(() => {
       removeFromFoods(food);
-    }, 10000);
-  }, 4000);
+    }, 7000);
+  }, 5000);
+
+  const gameInterval = setInterval(() => {
+    snake.move();
+    ctx.clearRect(0, 0, 800, 600);
+    drawFood();
+    snake.draw();
+    if (snake.dead()) {
+      window.clearInterval(gameInterval);
+    }
+  }, 100);
 
   function drawFood() {
-    for (let index in foods) {
-      const food = foods[index];
-
-      if (typeof food !== "undefined") {
+    for (var food_index in foods) {
+      const food = foods[food_index];
+      if (validFood(food)) {
         food.draw();
-        if (hit(food, snake.head)) {
-          snake.eat();
-          removeFromFoods(food);
-        }
+        didCollide(food);
       }
+    }
+  }
+
+  function validFood(food) {
+    return typeof food != "undefined";
+  }
+
+  function didCollide(food) {
+    if (validFood(food) && hit(food, snake.head)) {
+      snake.eat();
+      removeFromFoods(food);
     }
   }
 
   function removeFromFoods(food) {
     foods = foods.filter(function (f) {
-      return food !== f;
+      return f != food;
     });
+  }
+
+  function snakeHit(a, b) {
+    return a.x == b.x && b.y == a.y;
+  }
+
+  function hit(a, b) {
+    var hit = false;
+    //Horizontal collisions
+    if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+      //Vertical collisions
+      if (b.y + b.height >= a.y && b.y < a.y + a.height) hit = true;
+    }
+    //Collision a with b
+    if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+      if (b.y <= a.y && b.y + b.height >= a.y + a.height) hit = true;
+    }
+
+    //Collision b with a
+    if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+      if (a.y <= b.y && a.y + a.height >= b.y + b.height) hit = true;
+    }
+    return hit;
   }
 })();
